081e8884b6c616a4481a71931f9df0e9fc60abe1
ScotchCarbTTV <scotch.carb@gmail.com>
2022-06-23 20:13:26 +0800

    Implemented Signs
    #GAD170.1 Jumpboy World 64
    Summary:
    1. Imported the Sign model and Sign texture for UI panel
    2. Create Sign.cs and SignCanvas.cs and created code to allow player to interact and switch the camera's focus to the UI panel with the signs content
    3. Created the Sign prefab containing the model, world canvas, and UI elements

---
+++ Assets/Sign.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Sign : MonoBehaviour
+{
+    //variable to reference the Cinemachine Freelook Camera
+    [SerializeField] private CinemachineFreeLook freeLook;
+
+    //variable to control UI panel
+    [SerializeField] private GameObject panel;
+
+    //variable for the camera focal on the player
+    [SerializeField] private GameObject focal;
+    [SerializeField] private GameObject signFocal;
+    [SerializeField] private GameObject prompt;
+
+    bool inRange;
+
+    private void Start()
+    {        
+        GameObject.FindGameObjectWithTag("FreeLook").TryGetComponent<CinemachineFreeLook>(out freeLook);
+        focal = GameObject.FindGameObjectWithTag("FreelookFocal");
+        panel.SetActive(false);
+        inRange = false;
+        prompt.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && inRange == true)
+        {
+            TogglePanel();
+        }
+    }
+
+    public void TogglePanel()
+    {
+        if(panel.activeSelf == false)
+        {
+            panel.SetActive(true);
+            freeLook.LookAt = signFocal.transform;
+            prompt.SetActive(false);
+        }
+        else
+        {
+            panel.SetActive(false);
+            freeLook.LookAt = focal.transform;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = true;
+            prompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            inRange = false;
+            prompt.SetActive(false);
+            if(panel.activeSelf == true)
+            {
+                TogglePanel();
+            }
+        }
+    }
+
+}
---
+++ Assets/SignCanvas.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignCanvas : MonoBehaviour
+
+{    void Update()
+    {
+        //rotate the canvas to always face the main camera
+        transform.forward = Camera.main.transform.forward;
+    }
+}
